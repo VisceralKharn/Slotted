245024cf68cd11ef2d76dccb4c19306e90d8572c
VisceralKharn <79151242+VisceralKharn@users.noreply.github.com>
2022-09-15 09:28:09 -0400

    Create Zyra.lua

---
+++ Zyra.lua
@@ -0,0 +1,191 @@
+Combo_key = 1
+Clear_key = 3
+Harass_key = 4
+White = color:new(255,255,255)
+Red = color:new(255,0,0)
+Green = color:new(0,255,0)
+Blue = color:new(0,0,200)
+barrels = {}
+
+print(g_local.champion_name)
+
+function createEnemiesList()
+    return features.entity_list:get_enemies()
+end
+
+function createEnemyMinionsList()
+    return features.entity_list:get_enemy_minions()
+end
+
+function getTargetMr(target)
+    --return 100 / (target.MR+100)
+    return 100 / (target.total_mr + 100)
+end
+
+function getDistance(from,to)
+    return from:dist_to(to)
+end
+
+
+local mySpells = {
+    q = {
+        lastCast = 0,
+        manaCost = {55 , 50 , 45 , 40 , 35},
+        spell = g_local:get_spell_book():get_spell_slot(e_spell_slot.q),
+        spellSlot = e_spell_slot.q,
+        apRatio = 1,
+        Range = 950,
+        Width = 250,
+        Speed = 2600,
+        Level = 0,
+        Base = {60, 105, 150, 195, 240},
+        CastTime = 0.25,
+        coolDown = {4.5 , 4.5 , 4.5 , 4.5 , 4.5},
+        TotalDamage = 0 },
+    w = {
+        lastCast = 0,
+        manaCost = {60 , 70 , 80 , 90 , 100},
+        spell = g_local:get_spell_book():get_spell_slot(e_spell_slot.w),
+        spellSlot = e_spell_slot.w,
+        apRatio = .3,
+        Range = 650,
+        Width = 550,
+        Speed = 10000,
+        Level = 0,
+        Base = {80, 120, 160, 200, 240},
+        CastTime = 0.25,
+        TotalDamage = 0 },
+    e = {
+        lastCast = 0,
+        manaCost = {0,0,0,0,0},
+        spell = g_local:get_spell_book():get_spell_slot(e_spell_slot.e),
+        spellSlot = e_spell_slot.e,
+        apRatio = {.4,.45,.5,.55,.6},
+        Range = 500,
+        Width = 345,
+        Speed = 1600,
+        Level = 0,
+        Base = {50, 70, 90, 110, 130},
+        CastTime = 0.15,
+        TotalDamage = 0 },
+    r = {
+        lastCast = 0,
+        manaCost = {100, 100, 100},
+        spell = g_local:get_spell_book():get_spell_slot(e_spell_slot.r),
+        spellSlot = e_spell_slot.r,
+        apRatio = .5,
+        empoweredRatio = 1.8,
+        Range =  175 ,
+        Width = 360,
+        Speed = 1600,
+        Level = 0,
+        Base = {300, 475 , 650 },
+        CastTime = 0.15,
+        TotalDamage = 0 },
+    totalComboDamage = 0
+}
+function mySpells:refreshSpells()
+    self['q'].Level = g_local:get_spell_book():get_spell_slot(e_spell_slot.q).level
+    self['q'].spell = g_local:get_spell_book():get_spell_slot(e_spell_slot.q)
+    self['q'].spellSlot = e_spell_slot.q
+
+    self['w'].Level = g_local:get_spell_book():get_spell_slot(e_spell_slot.w).level
+    self['w'].spell = g_local:get_spell_book():get_spell_slot(e_spell_slot.w)
+    self['w'].spellSlot = e_spell_slot.w
+
+    self['e'].Level = g_local:get_spell_book():get_spell_slot(e_spell_slot.e).level
+    self['e'].spell = g_local:get_spell_book():get_spell_slot(e_spell_slot.e)
+    self['e'].spellSlot = e_spell_slot.e
+
+    self['r'].Level = g_local:get_spell_book():get_spell_slot(e_spell_slot.r).level
+    self['r'].spell = g_local:get_spell_book():get_spell_slot(e_spell_slot.r)
+    self['r'].spellSlot = e_spell_slot.r
+end
+
+
+function mySpells:isSpellReady(spell)
+    return self[spell].spell:is_ready()
+end
+
+
+function mySpells:haveEnoughMana(spell)
+    if g_local.mana >= self[spell].manaCost[self[spell].Level] then
+        return true
+    else
+        return false
+    end
+end
+
+
+function mySpells:canCast(spell)
+    self:refreshSpells()
+    if self:isSpellReady(spell) and self:haveEnoughMana(spell) then
+        return true
+    else
+        return false
+    end
+end
+
+
+function mySpells:castSpellOnTarget(spellToCast,target)
+    local target = target or nil
+    if self:canCast(spellToCast) then
+        local castSpellSlot = self[spellToCast].spellSlot
+        g_input:cast_spell((castSpellSlot), target)
+    end
+end
+
+
+function mySpells:castSpellLocation(spellToCast,location)
+    if self:canCast(spellToCast) then
+        local castSpellSlot = self[spellToCast].spellSlot
+        g_input:cast_spell((castSpellSlot), location)
+    end
+    return location
+end
+
+
+function mySpells:isSpellInRange(spell,target)
+    print(self[spell].Range)
+    if target.position:dist_to(g_local.position) <= self[spell].Range then
+        return true
+    else
+        return false
+    end
+end
+
+function mySpells:getSpellDamage(spell)
+    self:refreshSpells()
+    local currentBase = self[spell].Base[self[spell].Level]
+    return ( g_local:get_ability_power() * self[spell].apRatio) + currentBase
+end
+
+function mySpells:getZyraCasts()
+    local spellCastInfo = g_local:get_spell_book():get_spell_cast_info()
+    if spellCastInfo ~= nil then
+        local spellCastSlot = g_local:get_spell_book():get_spell_slot(spellCastInfo.slot)
+        if spellCastSlot ~= nil then
+            local spellSlotName = spellCastSlot:get_name()
+            if spellSlotName == 'ZyraE' or spellSlotName == 'ZyraQ' then
+                return true
+            end
+        end
+    end
+end
+
+
+function mySpells:wSpell()
+    local mode = features.orbwalker:get_mode()
+    if mode == Clear_key or mode == Harass_key or mode == Combo_key then
+        if self:getZyraCasts() == true then
+            self:castSpellLocation('w', features.target_selector:get_default_target().position)
+        end
+    end
+end
+
+wFunction = function()
+    mySpells:wSpell()
+    --mySpells:getZyraCasts()
+end
+
+cheat.register_callback("feature", wFunction)
